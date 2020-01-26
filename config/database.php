f3fb6e4e42a34f0ad5008e40dcd0fd32784cd7f6
Tom S <harib1278@users.noreply.github.com>
2020-01-26 13:54:11 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -5,9 +5,3 @@ define('DB_NAME', 'menagerie');
 define('DB_USER', 'root');
 define('DB_PASS', '#e[XZh4X>[');
 define('DB_HOST', 'localhost');
-
-/* bbk config left in for visual aid*/
-//define("DB_NAME", "tshadd01db");
-//define("DB_USER", "tshadd01");
-//define("DB_PASS", "bbkmysql");
-//define("DB_HOST", "mysqlsrv.dcs.bbk.ac.uk");
